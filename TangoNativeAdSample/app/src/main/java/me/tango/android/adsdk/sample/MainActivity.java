f58c1a736630c3daed976301ac50f777a3093d53
Artour Bakiev <ABakiev@tango.me>
2017-03-10 19:27:55 +0300

    The whole list of params added to ad query according to  MAD-8854 Leak canary library added TESTED: LG E980, Nexus 5X

--- TangoNativeAdSample/app/src/main/java/me/tango/android/adsdk/sample/MainActivity.java
+++ TangoNativeAdSample/app/src/main/java/me/tango/android/adsdk/sample/MainActivity.java
@@ -81,7 +81,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     RecyclerView.Adapter<RecyclerView.ViewHolder> adapter = null;
     if (!TextUtils.isEmpty(cid)) {
       final int items = random.nextInt(TITLES.length + 1) * (random.nextInt(50) + 50);
-      adapter = new LocalAdapter(items, new AdLoader(cid));
+      adapter = new LocalAdapter(items, new AdLoader(cid, null));
     }
     mRecyclerView.setAdapter(adapter);
   }
